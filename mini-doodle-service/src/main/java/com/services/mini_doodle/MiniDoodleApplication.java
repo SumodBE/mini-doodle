62e7b87ed1046d91e359532c57fc867b7513bd39
Sumod Badchhape <sumodbadchhape@Sumods-MacBook-Pro.local>
2026-01-26 19:46:38 +0530

    Initial commit - created the project

---
+++ mini-doodle-service/src/main/java/com/services/mini_doodle/MiniDoodleApplication.java
@@ -0,0 +1,11 @@
+package com.services.mini_doodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoodleApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MiniDoodleApplication.class, args);
+    }
+}
